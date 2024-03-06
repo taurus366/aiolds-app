8dc27442d147c5945a54ce43c82afd103c5545ef
Arcerr <taurus..ali47>
2024-03-06 17:55:15 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'aiolds.app',
+  appName: 'aiolds-app',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
